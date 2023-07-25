024fa1a01ba3dde116a7321565947b25258e3e1f
Asare foster <asarefosterinnocent@gmail.com>
2023-07-24 17:05:26 -0700

    0x11. C - printf team project

--- main.h
+++ main.h
@@ -42,9 +42,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 
-int handle_print(const char *fmt, int *i,
-
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /*** FUNCTIONS ***/
 
