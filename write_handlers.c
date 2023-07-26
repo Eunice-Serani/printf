acfbc9f5a21fbb609e0d1813f4b24feef5dc6c4f
Asare foster <asarefosterinnocent@gmail.com>
2023-07-25 19:01:11 -0700

    C - printf project

--- write_handlers.c
+++ write_handlers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* WRITE HANDLE *************************/
+/*********** WRITE HANDLE ***********/
 /**
  * handle_write_char - Prints a string
  * @c: char types.
@@ -44,7 +44,7 @@ int handle_write_char(char c, char buffer[],
 	return (write(1, &buffer[0], 1));
 }
 
-/************************* WRITE NUMBER *************************/
+/******** WRITE NUMBER ************/
 /**
  * write_number - Prints a string
  * @is_negative: Lista of arguments
