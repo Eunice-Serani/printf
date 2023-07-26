ed8b41d6cc489ee42da5fa4e4559f4baffc533d4
Asare foster <asarefosterinnocent@gmail.com>
2023-07-26 01:11:25 -0700

    C - printf project

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%') 
+		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
